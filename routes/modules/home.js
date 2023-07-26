4e3cda7bb2c79ebc69269df4ea1a77b0c2bf6ae8
Sunnylin0320 <sunny8503201@gmail.com>
2023-07-26 23:14:39 +0800

    feat:bootswatch

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const Record = require("../../models/record");
+const Category = require("../../models/category");
 
 //拿到所有的資料
-router.get("/", (req, res) => {
-  const userId = req.user._id;
-  Record.find({ userId })
-    .lean()
-    .sort({ date: "asc" })
-    .then((records) => res.render("index", { records }))
-    .catch((error) => console.error(error));
+router.get("/", async (req, res) => {
+  const categories = await Category.find().lean();
+  const categoryData = {};
+  categories.forEach(
+    (category) => (categoryData[category.name] = category.icon)
+  );
+
+  async function getAllData() {
+    const userId = req.user._id;
+    const records = await Record.find({ userId }).lean();
+    try {
+      let totalAmount = 0;
+      const date = [];
+
+      for (let i = 0; i < records.length; i++) {
+        if (!date.includes(records[i].date.slice(0, 7))) {
+          date.push(records[i].date.slice(0, 7));
+        }
+        records[i].category = categoryData[records[i].category];
+        totalAmount = totalAmount + records[i].amount;
+      }
+
+      res.render("index", { records, categories, totalAmount, date });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getAllData();
 });
 
 // 匯出路由模組
